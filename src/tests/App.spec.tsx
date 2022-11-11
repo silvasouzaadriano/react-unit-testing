057c2a9ac45145e013e6b88a358482dbd2bb7a47
adriano.souza <adriano.souza@zpesystems.com>
2022-11-11 19:46:22 -0300

    feat(getByText): Using getByText with toHaveAttribute

--- src/tests/App.spec.tsx
+++ src/tests/App.spec.tsx
@@ -4,5 +4,5 @@ import App from '../App';
 test('sum', () => {
   const { getByText } = render(<App />)
 
-  expect(getByText('Hello World!')).toBeInTheDocument()
+  expect(getByText('Hello World!')).toHaveAttribute('class', 'test')
 })
